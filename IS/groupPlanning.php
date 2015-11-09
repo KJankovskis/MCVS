cea4e542011df9ebd6741868ae9655a175bf1223
KJankovskis <krists.jankovskis@gmail.com>
2015-11-09 12:16:09 +0200

    Vizuāli labojumi lapa "groupPlanning.php"

--- IS/groupPlanning.php
+++ IS/groupPlanning.php
@@ -8,14 +8,14 @@
 <div class="gpContent">
     <table>
         <tr>
-            <td width="35%">
+            <td width="40%">
                 <label id="gpCourseLabel">Izvēlieties mācību kursu:</label>
             </td>
-            <td width="45%">
-                <select id="gpCourseList">
-                    <option name="000" value=""></option>
-                    <option name="001" value="Projektu vadība"></option>
-                    <option name="002" value="Ievads darbam ar sistēmu"></option>
+            <td width="40%">
+                <select id="gpCourseList" style="width: 300px; height: 30px">
+                    <option value="000"></option>
+                    <option value="001">Projektu vadība</option>
+                    <option value="002">Ievads darbam ar sistēmu</option>
                 </select>
             </td>
             <td width="20%">
@@ -24,14 +24,14 @@
         </tr>
         
         <tr>
-            <td width="35%">
+            <td width="40%">
                 <label id="gpTeacherLabel" style="height: 30px">Izvēlieties pasniedzēju:</label>
             </td>
-            <td width="45%">
-                <select id="gpTeacherList" style="height: 30px">
-                    <option name="000" value=""></option>
-                    <option name="001" value="Jānis Zariņš"></option>
-                    <option name="002" value="Indulis Celms"></option>
+            <td width="40%">
+                <select id="gpTeacherList" style="width: 300px; height: 30px">
+                    <option value="000"></option>
+                    <option value="001">Jānis Zariņš></option>
+                    <option value="002">Indulis Celms></option>
                 </select>
             </td>
             <td width="20%">
@@ -40,14 +40,14 @@
         </tr>
         
         <tr>
-            <td width="35%">
+            <td width="40%">
                 <label id="gpRoomInfo" style="height: 30px">Izvēlieties auditoriju:</label>
             </td>
-            <td width="45%">
+            <td width="40%">
                 <select id="gpRoomList" style="height: 30px">
-                    <option name="000" value=""></option>
-                    <option name="001" value="Liepāja, Dzintaru iela 1, 34. auditorija"></option>
-                    <option name="002" value="Rīga, Bērzu iela 2, PARĪZE"></option>
+                    <option value="000"></option>
+                    <option value="001">Liepāja, Dzintaru iela 1, 34. auditorija</option>
+                    <option value="002">Rīga, Bērzu iela 2, PARĪZE</option>
                 </select>
             </td>
             <td width="20%">
@@ -56,11 +56,11 @@
         </tr>
         
         <tr>
-            <td width="35%">
+            <td width="40%">
                 <label id="gpRoomLabel">Ievadiet sākuma un beigu datumus:</label>
             </td>
-            <td width="45%">
-                <input type="date" id="gpDateFrom"> - <input type="date" id="gpDateTo">
+            <td width="40%">
+                <input type="date" id="gpDateFrom" style="height: 30px; width: 125px"> - <input type="date" id="gpDateTo" style="height: 30px; width: 125px">
             </td>
             <td width="20%">
                 
@@ -68,11 +68,11 @@
         </tr>
         
         <tr>
-            <td rowspan="3" width="35%">
+            <td rowspan="3" width="40%">
                 <label id="gpStudentLabel">Pievienojiet studentus:</label>
             </td>
-            <td width="45%">
-                <input type="text" id="gpStudentName" placeholder="Studenta vārds">
+            <td width="40%">
+                <input type="text" id="gpStudentName" placeholder="Studenta vārds" style="height: 30px; width: 300px">
             </td>
             <td rowspan="3" width="20%">
                 <input type="submit" id="gpStudentSearchButton" value="Meklēt">
@@ -80,23 +80,25 @@
         </tr>
         
         <tr>
-            <td width="45%">
-                <input type="text" id="gpStudentSurname" placeholder="Studenta uzvārds">
+            <td width="40%">
+                <input type="text" id="gpStudentSurname" placeholder="Studenta uzvārds" style="height: 30px; width: 300px">
             </td>
         </tr>
         
         <tr>
-            <td width="45%">
-                <input type="text" id="gpStudentID" placeholder="Studenta personas kods">
+            <td width="40%">
+                <input type="text" id="gpStudentID" placeholder="Studenta personas kods" style="height: 30px; width: 300px">
             </td>
         </tr>
     </table>
     <br>
     <div class="gpContentLeft">
         <label id="gpCourseResultLabel" style="height: 30px">Izvēlētais kurss:</label>
+        <label id="gpTeacherResultLabel" style="height: 30px">Izvēlētais pasniedzējs:</label>
     </div>
     <div class="gpContentRight">
         <label id="gpCourseResultInfo" style="height: 30px">Projektu vadība</label>
+        <label id="gpTeacherResultInfo" style="height: 30px">Arnis Strautiņš</label>
     </div>
     <br>
     <center><input type="submit" id="gpCreateButton" value="IZVEIDOT MĀCĪBU GRUPU"></center>
