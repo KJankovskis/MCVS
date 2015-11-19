f0340e7c8e93e77577784c51e975636307cc3002
KJankovskis <krists.jankovskis@gmail.com>
2015-11-20 01:56:26 +0200

    Labojumi newUser.php lapā

--- IS/newUser.php
+++ IS/newUser.php
@@ -15,27 +15,42 @@
     <div class="middleUser">
         <form action="newUser.php" method="post" enctype="multipart/form-data">
             <div class = "forTextFields">
-                <label>Vārds: </label><input type="text" name="vards" /><br><br>
-                <label>Uzvards: </label><input type="text" name="uzvards" /><br><br>
-                <label>E-pasts: </label><input type="text" name="epasts" /><br><br>
-                <label>Tālrunis: </label><input type="text" name="talrunis" /><br><br>
-                <label>Perosnas kods: </label><input type="text" name="personasKods" /><br><br>
-                <label>Dzīves vietas adrese: </label><input type="text" name="dzivesAdrese" /><br><br>
-                <label>Dzīves vietas pilsēta: </label><input type="text" name="dzivesPilseta" /><br><br> 
-                <label>Darba vietas adrese:: </label><input type="text" name="darbaAdrese" /><br><br>
-                <label>Darba vietas pilsēta: </label><input type="text" name="darbaPilseta" /><br><br>
-                <label>Personas foto: </label><input type="file" name="foto" id="foto"><br><br>
-                <label>Lietotājvārds: </label><input type="text" name="lietotajvards" /><br><br>
-                <label>Parole: </label><input type="text" name="parole" /><br><br>
-                <label>Lietotāja loma: </label><br><select name="lietotajaLoma" id="lietotajaLoma">
+                <div class="forTextFieldsL">
+                    <label id="newUserVards" style="height: 35px">Vārds:</label><br>
+                    <label style="height: 35px">Uzvārds:</label><br>
+                    <label style="height: 35px">E-pasts:</label><br>
+                    <label style="height: 35px">Tālrunis:</label><br>
+                    <label style="height: 35px">Personas kods:</label><br>
+                    <label style="height: 35px">Dzīves vietas adrese:</label><br>
+                    <label style="height: 35px">Dzīves vietas pilsēta:</label><br>
+                    <label style="height: 35px">Darba vietas adrese:</label><br>
+                    <label style="height: 35px">Dzīves vietas pilsēta:</label><br>
+                    <label style="height: 35px">Personas foto:</label><br>
+                    <label style="height: 35px">Lietotājvārds:</label><br>
+                    <label style="height: 35px">Parole:</label><br>
+                    <label style="height: 35px">Lietotāja loma:</label><br>
+                </div>
+                <div class="forTextFieldsR">
+                    <input id="newUserName" type="text" name="vards"><br>
+                    <input id="newUserSurname"  type="text" name="uzvards"><br>
+                    <input id="newUserEmail" type="text" name="epasts"><br>
+                    <input id="newUserPhone" type="text" name="talrunis"><br>
+                <input id="newUserPK" type="text" name="personasKods"><br>
+                <input id="newUserHomeAddress" type="text" name="dzivesAdrese"><br>
+                <input id="newUserHomeCity" type="text" name="dzivesPilseta"><br> 
+                <input id="newUserWorkAddress" type="text" name="darbaAdrese"><br>
+                <input id="newUserWorkCity" type="text" name="darbaPilseta"><br>
+                <input id="newUserPicture" type="file" name="foto" id="foto"><br>
+                <input id="newUserUsername" type="text" name="lietotajvards"><br>
+                <input id="newUserPassword" type="text" name="parole"><br>
+                <select name="lietotajaLoma" id="lietotajaLoma">
                     <option value="L" name="L">Lietotājs</option>
                     <option value="P" name="P">Pasniedzējs</option>
                     <option value="A" name="A">Administrators</option>
-                </select><br>
+                </select>
+                </div>
             </div>
-            <?php         
-                print "<input class=\"saveButton\" type=\"Submit\" name=\"Submit\" value=\"Pievienot\"> ";
-            ?>
+            <center><input id="newUserButton" class="newUserSaveButton" type="Submit" name="Submit" value="Izveidot lietotāju"></center>
         </form>
     </div> 
 <?php
