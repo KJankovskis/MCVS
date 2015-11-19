d06696a61c1e3f9325963881dba9f5e8faf1e506
KJankovskis <krists.jankovskis@gmail.com>
2015-11-19 17:12:51 +0200

    Labojumi newUser.php lapā V1

--- IS/newUser.php
+++ IS/newUser.php
@@ -15,26 +15,36 @@
     <div class="middleUser">
         <form action="newUser.php" method="post" enctype="multipart/form-data">
             <div class = "forTextFields">
-                <label>Vārds: </label><input type="text" name="vards" /><br><br>
-                <label>Uzvards: </label><input type="text" name="uzvards" /><br><br>
-                <label>E-pasts: </label><input type="text" name="epasts" /><br><br>
-                <label>Tālrunis: </label><input type="text" name="talrunis" /><br><br>
-                <label>Perosnas kods: </label><input type="text" name="personasKods" /><br><br>
-                <label>Dzīves vietas adrese: </label><input type="text" name="dzivesAdrese" /><br><br>
-                <label>Dzīves vietas pilsēta: </label><input type="text" name="dzivesPilseta" /><br><br> 
-                <label>Darba vietas adrese:: </label><input type="text" name="darbaAdrese" /><br><br>
-                <label>Darba vietas pilsēta: </label><input type="text" name="darbaPilseta" /><br><br>
-                <label>Personas foto: </label><input type="file" name="foto" id="foto"><br><br>
-                <label>Lietotājvārds: </label><input type="text" name="lietotajvards" /><br><br>
-                <label>Parole: </label><input type="text" name="parole" /><br><br>
-                <label>Lietotāja loma: </label><br><select name="lietotajaLoma" id="lietotajaLoma">
+                <div class="forTextFieldsL">
+                    Vārds:<br>
+                    Uzvārds:<br>
+                    E-pasts:
+                </div>
+                <div clas="forTextFieldsR">
+                    <input id="newUserName" type="text" name="vards"><br>
+                    <input id="newUserSurame"  type="text" name="uzvards"><br>
+                    <input id="newUserEmail" type="text" name="epasts"><br>
+                </div>
+                Vārds: <input id="newUserName" type="text" name="vards"><br>
+                Uzvards: <input id="newUserSurame"  type="text" name="uzvards"><br>
+                E-pasts: <input id="newUserEmail" type="text" name="epasts"><br>
+                Tālrunis: <input id="newUserPhone" type="text" name="talrunis"><br>
+                Personas kods: <input id="newUserPK" type="text" name="personasKods"><br>
+                Dzīves vietas adrese: <input id="newUserHomeAddress" type="text" name="dzivesAdrese"><br>
+                Dzīves vietas pilsēta: <input id="newUserHomeCity" type="text" name="dzivesPilseta"><br> 
+                Darba vietas adrese:: <input id="newUserWorkAddress" type="text" name="darbaAdrese"><br>
+                Darba vietas pilsēta: <input id="newUserWorkCity" type="text" name="darbaPilseta"><br>
+                Personas foto: <input id="newUserPicture" type="file" name="foto" id="foto"><br>
+                Lietotājvārds: <input id="newUserUsername" type="text" name="lietotajvards"><br>
+                Parole: <input id="newUserPassword" type="text" name="parole"><br><br>
+                Lietotāja loma: <select name="lietotajaLoma" id="lietotajaLoma">
                     <option value="L" name="L">Lietotājs</option>
                     <option value="P" name="P">Pasniedzējs</option>
                     <option value="A" name="A">Administrators</option>
                 </select><br>
             </div>
             <?php         
-                print "<input class=\"saveButton\" type=\"Submit\" name=\"Submit\" value=\"Pievienot\"> ";
+                print "<input class=\"saveButton\" type=\"Submit\" name=\"Submit\" value=\"Izveidot lietotāju"\"> ";
             ?>
         </form>
     </div> 
