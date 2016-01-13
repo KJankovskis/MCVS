59280f616e5dbbe026653ba93e344b5cd48a32cf
KJankovskis <krists.jankovskis@gmail.com>
2016-01-13 19:25:28 +0200

    pēdējais mazais labojums

--- IS/searchGroup.php
+++ IS/searchGroup.php
@@ -87,7 +87,7 @@ else{
 		<br>
 		<br>
 		<br>
-        <p><?php echo "<b>Pasniedzejs</b>:  $pasniedzejaVards $pasniedzejaUzvards , $pasniedzejaPersonasKods" ?></p>
+        <p><?php echo "<b>Pasniedzejs</b>:  $pasniedzejaVards $pasniedzejaUzvards, $pasniedzejaPersonasKods" ?></p>
         <p><?php echo "<b>Apgūstamais kurss</b>: $kursaNosaukums" ?></p>
         <p><?php echo "<b>Auditorija</b>: $auditorijasNosaukums ($aAdrese, $aPilseta)" ?></p>
         <p><?php echo "<b>Apmācību ilgums</b>: no $mgDatumsNo līdz $mgDatumsLidz" ?></p>     
