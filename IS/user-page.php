975770b9583589160a45be184d1746dc59eda3cf
KJankovskis <krists.jankovskis@gmail.com>
2016-01-13 17:23:23 +0200

    user-page V1.1

--- IS/user-page.php
+++ IS/user-page.php
@@ -39,11 +39,11 @@ else{
     <div class="person">
         <div class="profilePicture">
             <?php
-echo '<dd>'
+echo //'<dd>'
      //. '<object data="atteli/defaultPerson.png" type="image/png">'
-     .      '<img src="data:image/jpeg;base64,' . base64_encode($foto) . '" width="200" height="230">'
+           '<img src="data:image/jpeg;base64,' . base64_encode($foto) . '" width="200" height="230">';
      //. '</object>'
-     . '</dd>';
+     //. '</dd>';
 ?>
             
         </div>
