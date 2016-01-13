ee758ac250a2308acfc47ec24a621b47586c944b
ArnisFreimanis <freimanis15@gmail.com>
2016-01-13 16:37:45 +0200

    Revert "Personas noslogojuma tabula - V1.8"
    This reverts commit 0b71fb3500c000da12cebe3a0f89e2b4a6cab436.

--- IS/searchProfile.php
+++ IS/searchProfile.php
@@ -37,13 +37,13 @@ else{
                 $role = $rows['lietotajaLoma'];
             }
         }
-       /* $resultNoslogojums=$mysqli->query("SELECT * FROM Persona_has_MacibuGrupa WHERE Persona_idPersona='$ID' ");
+        $resultNoslogojums=$mysqli->query("SELECT * FROM Persona_has_MacibuGrupa WHERE Persona_idPersona='$ID' ");
 		if($resultNoslogojums->num_rows !=0){
             while($rows = $resultNoslogojums->fetch_assoc()){ 
                 $IDmGrupa = $rows['MacibuGrupa_idMacibuGrupa'];
             }
         }
-		$resultMacibuGrupa=$mysqli->query("SELECT * FROM MacibuGrupa WHERE 	idMacibuGrupa='$IDmGrupa'");*/
+		$resultMacibuGrupa=$mysqli->query("SELECT * FROM MacibuGrupa WHERE 	idMacibuGrupa='$IDmGrupa'");
     ?>
     <div class="name-surname">
         <p ><?php echo "$name $surname"; ?> </p>
@@ -78,33 +78,25 @@ echo '<dd>'
     
     <div class="about">
         <div class="noslogojums"><p>Noslogojums</p></div>
-        <table style="width:100%; border: 1px solid black; border-collapse: collapse; position: absolute; top:30px;">
+        <table style="width:100%; border: 1px solid black; border-collapse: collapse;">
           <tr>
                 <th style="padding: 5px; border: 1px solid black; border-collapse: collapse;">Nr.</th>	
 				<th style="padding: 5px; border: 1px solid black; border-collapse: collapse;">Aktivitāte</th>
 				<th style="padding: 5px; border: 1px solid black; border-collapse: collapse;">Datums no</th>
 				<th style="padding: 5px; border: 1px solid black; border-collapse: collapse;">Datums līdz</th>
-		  </tr>
+          </tr>
           <tr>
         <?php
         $tmp = 0;
         $x = 0;
-		
-		$resultNoslogojums=$mysqli->query("SELECT * FROM Persona_has_MacibuGrupa WHERE Persona_idPersona='$ID' ");
-		if($resultNoslogojums->num_rows !=0){
-            while($rows = $resultNoslogojums->fetch_assoc()){ 
-                $IDmGrupa = $rows['MacibuGrupa_idMacibuGrupa'];
-				$resultMacibuGrupa=$mysqli->query("SELECT * FROM MacibuGrupa WHERE 	idMacibuGrupa='$IDmGrupa'");
-					if ($resultMacibuGrupa->num_rows > 0) {
-						while($row = $resultMacibuGrupa->fetch_assoc()) {
-							$tmp = $tmp +1;
-							echo "<td><center>" . $tmp. "</center></td><td><center>" . $row["mGrupasNosaukums"]. "</center></td><td><center>" . $row["mgDatumsNo"]. "</center></td><td><center>" . $row["mgDatumsLidz"]."</center></td></tr>". "<br>";
-						}
-					} else {
-						$x = 404;
-					}
+        if ($resultMacibuGrupa->num_rows > 0) {
+                while($row = $resultMacibuGrupa->fetch_assoc()) {
+                    $tmp = $tmp +1;
+                    echo "<td><center>" . $tmp. "</center></td><td><center>" . $row["mGrupasNosaukums"]. "</center></td><td><center>" . $row["mgDatumsNo"]. "</center></td><td><center>" . $row["mgDatumsLidz"]."</center></td></tr>". "<br>";
+                }
+            } else {
+                $x = 404;
             }
-        }
         ?>     
         </table>
         <br><br><br><br><center>
